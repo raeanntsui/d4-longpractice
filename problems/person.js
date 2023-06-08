aa585b32195df995fbc66ed63710d6d96c6d5dfa
Jomar Yanos <jomaryanos@gmail.com>
2023-06-08 16:32:12 -0700

    2nd commit

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,18 @@
 class Person {
   // Your code here
+  constructor(name, age) {
+    this.name = name
+    this.age = age;
+  }
+
+  sayHello () {
+    return `Hello, ${this.name}.`
+  }
+
+
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson.name}`
+  }
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,38 @@
 // Your code here
+const chai = require("chai");
+const expect = chai.expect;
+
+const Person = require("../problems/person");
+
+// describe('Person', () => {
+//     it('property exist in instance', () => {
+//         expect(this.name).to.be.an.instanceOf(Person)
+//     })
+// } )
+let person;
+let otherPerson;
+beforeEach(() => {
+  person = new Person("john", 45);
+  otherPerson = new Person("malik", 85);
+});
+
+describe("Person constructor", () => {
+
+  it("should set the name property and age", () => {
+    expect(person).to.have.property("name");
+    expect(person).to.have.property("age");
+  });
+
+  describe('sayHello', () => {
+    it('should say hello to person', () => {
+        expect(person.sayHello()).to.equal(`Hello, john.`)
+        expect(otherPerson.sayHello()).to.equal(`Hello, malik.`)
+    })
+  })
+
+  describe('visit otherperson', () => {
+    it('should return a string that person visted other person', () => {
+        expect(person.visit(otherPerson)).to.equal(`john visited malik`)
+    })
+})
+})
